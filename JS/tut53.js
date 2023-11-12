709488b3b0db69a19bf4df5574d90b81c7c3ce14
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-11-12 11:37:27 +0530

    Javascript functions

--- JS/tut53.js
+++ JS/tut53.js
@@ -1,3 +1,25 @@
 console.log("This is tutorial 53");
 
 // functions
+
+function greet(name){
+    console.log(name+" happy Diwali");
+}
+
+let name1 = "Nikhlesh"; 
+let name2 = "Sachin"; 
+let name3 = "Sandeep";
+let name4 = "Nikhil"; 
+
+greet(name1);
+greet(name2);
+greet(name3);
+greet(name4);
+
+function sum(a,b,c){
+    let d = a+b+c;
+    return d;
+}
+
+let returnVal = sum(5,11,4);
+console.log(returnVal);
